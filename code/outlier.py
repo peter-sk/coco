fb67dbeee796aa5cbdebc6d8a1da5d50241f4a2e
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2021-02-23 21:48:51 +0100

    * write info to txt file

--- code/outlier.py
+++ code/outlier.py
@@ -23,22 +23,27 @@ def main():
             height, width, _ = img.shape
             #print(txt_fname,width,height)
             f = open(txt_fname,"rt")
+            g = None
             takeIt = False
             for line in f.readlines():
                 coords = line.split()
                 #print(coords[1:5])
-                takeIt = True
                 x,y,w,h = convert2absolute(width,height,coords[1:5])
                 confidence = float(coords[5])
                 if w*h/width/height < 0.05 or confidence >= 30:
                     continue
-                print(img_fname.split(".jpg")[0]+".outlier.jpg",w,h,width,height,confidence)
+                takeIt = True
+                if g is None:
+                    g = open(".".join(txt_fname.split(".")[:-2])+".outlier.txt","wt")
+                g.write(line)
                 #print(x,y,w,h)
                 ul = int(x-w/2),int(y-h/2)
                 dr = int(x+w/2),int(y+h/2)
                 #print(ul,dr)
                 bbox = cv2.rectangle(img,ul,dr,(255,255,0),3)
             f.close()
+            if g is not None:
+                g.close()
             if takeIt:
                 selected += 1
                 cv2.imwrite(join(img_fname.split(".jpg")[0]+".outlier.jpg"),img)
